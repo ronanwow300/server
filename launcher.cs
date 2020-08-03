bd9d9bb4f2633593abb45e133b820ac2f07f784e
ronanwow300 <68086896+ronanwow300@users.noreply.github.com>
2020-08-02 23:35:53 -0700

    Create launcher.cs

---
+++ launcher.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace PlayToontownRemodified
+{
+   public void PlayGame()
+   {
+       Process.Start("game.exe")
+       }
+          Process.Start(url);
+          }
+     }
+}
